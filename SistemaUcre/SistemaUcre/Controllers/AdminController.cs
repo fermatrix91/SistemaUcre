3eedf49fa6660cc467de8e37278edd2748bf21e6
fermatrix91 <fjosuemn@gmail.com>
2016-11-08 23:51:18 -0600

    Ucre Recursos
    Go Ucre Recursos

--- SistemaUcre/SistemaUcre/Controllers/AdminController.cs
+++ SistemaUcre/SistemaUcre/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using SistemaUcre.Models.DAL;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -380,6 +381,264 @@ namespace SistemaUcre.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+
+        #endregion
+
+        #region Recursos
+        [Authorize]
+        public ActionResult Recursos(int idModuloActual)
+        {
+            if (User.IsInRole("Admin"))
+            {
+                Administrador adminActual = modeloUcre.Administrador.Where(x => x.Username == User.Identity.Name).FirstOrDefault();
+                List<Recurso> listaRecursos = new List<Recurso>();
+                listaRecursos = modeloUcre.Recurso.Where(x => x.IdModulo == idModuloActual).ToList();
+
+                Modulo moduloActual = new Modulo();
+                moduloActual = modeloUcre.Modulo.Find(idModuloActual);
+                ViewBag.NombreModulo = moduloActual.Nombre;
+                ViewBag.IdDeModulo = idModuloActual;
+
+                Dictionary<int, string> dicTipoRecursos = new Dictionary<int, string>();
+                List<TipoRecurso> listaTipoRecursos = new List<TipoRecurso>();
+                //listaTipoRecursos = modeloUcre.TipoRecurso.ToList();
+
+                foreach (var item in listaTipoRecursos)
+                {
+                    dicTipoRecursos.Add(item.IdTipoRecurso, item.Nombre);
+                }
+
+                ViewBag.ListaTiposRecursos = dicTipoRecursos;
+
+                ViewBag.NombreUsuario = adminActual.Nombre;
+                return View(listaRecursos);
+            }
+            WebSecurity.Logout();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult ParcialEditarRecurso(int? idRecurso)
+        {
+            if (User.IsInRole("Admin"))
+            {
+                Recurso recursoTemp = new Recurso();
+                recursoTemp = modeloUcre.Recurso.Find(Convert.ToInt32(idRecurso));
+
+                string nombreArchivo;
+                if (string.IsNullOrEmpty(recursoTemp.Urlarchivo))
+                    nombreArchivo = "Sin Archivo";
+                else
+                {
+                    var temp = recursoTemp.Urlarchivo.Split('/');
+                    nombreArchivo = temp[temp.Length - 1];
+                }
+
+                return Json(new
+                {
+                    Imagen = recursoTemp.Urlimagen,
+                    Archivo = recursoTemp.Urlarchivo,
+                    Descripcion = recursoTemp.Descripcion,
+                    NombreArchivo = nombreArchivo,
+                    IdRecurso = idRecurso
+
+                }, JsonRequestBehavior.AllowGet);
+            }
+            WebSecurity.Logout();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult GuardarRecurso(int idModulo, HttpPostedFileBase nombreImagen, HttpPostedFileBase nombreDocumento, string descripcion, string moduloCurso)
+        {
+            if (User.IsInRole("Admin"))
+            {
+                Recurso recursoTemp = new Recurso();
+
+                string urlImagen = "/Content/Asignaturas/";
+                string urlDocumento = "/Content/Asignaturas/";
+
+                Modulo moduloActual = modeloUcre.Modulo.Find(idModulo);
+
+                string idAsignaturaActual = moduloActual.IdAsignatura.ToString();
+                string idModuloActual = moduloActual.IdModulo.ToString();
+
+                urlImagen += idAsignaturaActual + "/Modulos/";
+                urlDocumento += idAsignaturaActual + "/Modulos/";
+
+                var rutaImagenTemp = urlImagen + idModuloActual;
+                var rutaDocTemporal = urlDocumento + idModuloActual;
+
+                string physicalPath = Server.MapPath("~" + rutaImagenTemp);
+                string pathTemporal = Path.Combine(physicalPath);
+                Directory.CreateDirectory(pathTemporal);
+
+                urlImagen = urlImagen + idModuloActual + '/' + nombreImagen.FileName;
+                urlDocumento = urlDocumento + idModuloActual + '/' + nombreDocumento.FileName;
+
+                recursoTemp.Urlimagen = urlImagen;
+                recursoTemp.Urlarchivo = urlDocumento;
+                recursoTemp.IdModulo = idModulo;
+                recursoTemp.Descripcion = descripcion;
+                recursoTemp.Estado = true;
+
+                String var = nombreDocumento.FileName;
+                int tam_var = var.Length;
+                String Var_Sub = var.Substring((tam_var - 4), 4);
+
+                recursoTemp.IdTipoRecurso = Var_Sub == ".pdf" ? 1 : 2;
+
+                modeloUcre.Recurso.Add(recursoTemp);
+                modeloUcre.Entry(recursoTemp).State = System.Data.EntityState.Added;
+                modeloUcre.SaveChanges();
+
+                var pathImagen = Path.Combine(Server.MapPath(rutaImagenTemp + '/'), nombreImagen.FileName);
+                nombreImagen.SaveAs(pathImagen);
+
+                var pathArchivo = Path.Combine(Server.MapPath(rutaDocTemporal + '/'), nombreDocumento.FileName);
+                nombreDocumento.SaveAs(pathArchivo);
+
+                return RedirectToAction("Recursos", new { idModuloActual = idModulo });
+            }
+            WebSecurity.Logout();
+            return RedirectToAction("Login", "Account");
+        }
+
+
+        public ActionResult EditandoRecurso(int idModulo, HttpPostedFileBase nombreImagen, HttpPostedFileBase nombreDocumento, string descripcion, string moduloCurso, int? idRecurso)
+        {
+            if (User.IsInRole("Admin"))
+            {
+                Recurso recursosTemp = new Recurso();
+                recursosTemp = modeloUcre.Recurso.Find(idRecurso);
+
+                string urlImagen = "/Content/Asignaturas/";
+                string urlDocumento = "/Content/Asignaturas/";
+
+                Modulo moduloActual = modeloUcre.Modulo.Find(idModulo);
+
+                string idAsignaturaActual = moduloActual.IdAsignatura.ToString();
+                string idModuloActual = moduloActual.IdModulo.ToString();
+
+                urlImagen += idAsignaturaActual + "/Modulos/";
+                urlDocumento += idAsignaturaActual + "/Modulos/";
+
+                var rutaImagenTemp = urlImagen + idModuloActual;
+                var rutaDocTemporal = urlDocumento + idModuloActual;
+
+                string physicalPath = Server.MapPath("~" + rutaImagenTemp);
+                string pathTemporal = Path.Combine(physicalPath);
+                Directory.CreateDirectory(pathTemporal);
+
+                if (nombreImagen != null)
+                {
+                    urlImagen = urlImagen + idModulo + '/' + nombreImagen.FileName;
+
+                    BorrarImagenRecurso(recursosTemp);
+                    recursosTemp.Urlimagen = urlImagen;
+
+                }
+                if (nombreDocumento != null)
+                {
+                    urlDocumento = urlDocumento + idModulo + '/' + nombreDocumento.FileName;
+
+                    BorrarArchivoRecurso(recursosTemp);
+                    recursosTemp.Urlarchivo = urlDocumento;
+                }
+
+                recursosTemp.Descripcion = descripcion;
+                String var = nombreDocumento.FileName;
+                int tam_var = var.Length;
+                String Var_Sub = var.Substring((tam_var - 4), 4);
+
+                recursosTemp.IdTipoRecurso = Var_Sub == ".pdf" ? 1 : 2;
+                //recursosTemp.estado = true;
+
+                modeloUcre.Recurso.Attach(recursosTemp);
+                modeloUcre.Entry(recursosTemp).State = System.Data.EntityState.Modified;
+                modeloUcre.SaveChanges();
+
+                if (nombreImagen != null)
+                {
+                    var pathImagen = Path.Combine(Server.MapPath(rutaImagenTemp + '/'), nombreImagen.FileName);
+                    nombreImagen.SaveAs(pathImagen);
+                }
+
+                if (nombreDocumento != null)
+                {
+                    var pathArchivo = Path.Combine(Server.MapPath(rutaDocTemporal + '/'), nombreDocumento.FileName);
+                    nombreDocumento.SaveAs(pathArchivo);
+                }
+                return RedirectToAction("Recursos", new { idModuloActual = idModulo });
+            }
+
+            WebSecurity.Logout();
+            return RedirectToAction("Login", "Account");
+        }
+
+        public ActionResult HabilitarRecurso(int idRecursoActual, int idModuloActual, bool estado)
+        {
+            if (User.IsInRole("Admin"))
+            {
+                Recurso recursoEntity = new Recurso();
+
+                recursoEntity = modeloUcre.Recurso.Find(Convert.ToInt32(idRecursoActual));
+
+                if (recursoEntity != null)
+                {
+                    recursoEntity.Estado = estado;
+
+                    modeloUcre.Recurso.Attach(recursoEntity);
+                    modeloUcre.Entry(recursoEntity).State = System.Data.EntityState.Modified;
+                    modeloUcre.SaveChanges();
+                }
+
+                return RedirectToAction("Recursos", new { idModuloActual = idModuloActual });
+            }
+            WebSecurity.Logout();
+            return RedirectToAction("Login", "Account");
+        }
+
+        public ActionResult EliminaRecurso(int idModuloActual, int idRecursoActual)
+        {
+            if (User.IsInRole("Admin"))
+            {
+                Recurso recursoEntity = new Recurso();
+                recursoEntity = modeloUcre.Recurso.Find(Convert.ToInt32(idRecursoActual));
+
+                if (recursoEntity != null)
+                {
+                    //recursoEntity.estado = true;
+
+                    BorrarImagenRecurso(recursoEntity);
+                    BorrarArchivoRecurso(recursoEntity);
+
+                    modeloUcre.Recurso.Attach(recursoEntity);
+                    modeloUcre.Entry(recursoEntity).State = System.Data.EntityState.Deleted;
+                    modeloUcre.SaveChanges();
+                }
+
+                return RedirectToAction("Recursos", new { idModuloActual = idModuloActual });
+            }
+            WebSecurity.Logout();
+            return RedirectToAction("Login", "Account");
+        }
+
+        private void BorrarImagenRecurso(Recurso recurso)
+        {
+            string fullPath = Request.MapPath("~" + recurso.Urlimagen);
+            if (System.IO.File.Exists(fullPath))
+            {
+                System.IO.File.Delete(fullPath);
+            }
+        }
+
+        private void BorrarArchivoRecurso(Recurso recurso)
+        {
+            string fullPath = Request.MapPath("~" + recurso.Urlarchivo);
+            if (System.IO.File.Exists(fullPath))
+            {
+                System.IO.File.Delete(fullPath);
+            }
+        }
 
         #endregion
 
